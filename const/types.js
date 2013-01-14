4532f8717db36bc806c381206a608cd7004558b7
Benjamin Goering <bengoering@gmail.com>
2013-01-13 21:41:14 -0800

    Document const/*

--- const/types.js
+++ const/types.js
@@ -1,11 +1,19 @@
-/*
- * The StreamHub APIs use enumerations to define
- * the type of message sent down the wire.
- * e.g. Content is comments and posts and stuff
- * TODO: These should be in the SDK
- *       That will be released in Jan/Feb 2013
- */
-define({
+define(
+/**
+The StreamHub APIs use enumerations to define
+the type of message sent down the wire. All types
+should be in this enumeration.
+TODO: These should be in the SDK
+      That will be released in Jan/Feb 2013
+@enum types
+@namespace types
+@property {Number} types.CONTENT - The good stuff. Juicy Content like comments
+@property {Number} types.OPINE - A user's opinion or something
+@property {Number} types.SHARE - TODO: I don't know yet.
+@property {Number} types.OEMBED - A new attachment
+@readonly
+*/
+{
 CONTENT: 0,
 OPINE: 1,
 SHARE: 2,
